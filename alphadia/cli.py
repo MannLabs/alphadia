395e70ac460c2c564f9538dc85f463683bfae7a7
Sander Willems <swillems89@hotmail.com>
2021-09-17 15:51:47 +0200

    FEAT: Quick functional GUI

--- alphadia/cli.py
+++ alphadia/cli.py
@@ -1,6 +1,10 @@
 #!python
 
 
+# builtin
+import logging
+import time
+
 # external
 import click
 
@@ -29,3 +33,106 @@ def run(ctx, **kwargs):
 def gui():
     import alphadia.gui
     alphadia.gui.run()
+
+
+@run.command(
+    "annotate",
+    help="Annotate a DIA file with an AlphaPept result library."
+)
+@click.argument(
+    "dia_file_name",
+    type=click.Path(exists=True, file_okay=True, dir_okay=True),
+    required=True,
+)
+@click.argument(
+    "alphapept_library_file_name",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    required=True,
+)
+@click.argument(
+    "output_file_name",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False),
+    required=True,
+)
+@click.option(
+    "--ppm_tolerance",
+    help="The ppm tolerance",
+    type=float,
+    default=20,
+    show_default=True,
+)
+@click.option(
+    "--rt_tolerance",
+    help="The rt tolerance in seconds",
+    type=float,
+    default=30,
+    show_default=True,
+)
+@click.option(
+    "--mobility_tolerance",
+    help="The mobility tolerance in 1/k0 (ignored for Thermo)",
+    type=float,
+    default=0.05,
+    show_default=True,
+)
+@click.option(
+    "--fdr_rate",
+    help="The FDR",
+    type=float,
+    default=0.01,
+    show_default=True,
+)
+@click.option(
+    "--thread_count",
+    help="The number of threads to use",
+    type=int,
+    default=-1,
+    show_default=True,
+)
+@click.option(
+    "--max_scan_difference",
+    help="KEEP DEFAULT",
+    type=int,
+    default=1,
+    show_default=True,
+)
+@click.option(
+    "--max_cycle_difference",
+    help="KEEP DEFAULT",
+    type=int,
+    default=1,
+    show_default=True,
+)
+def annotate(**kwargs):
+    logging.basicConfig(
+        format='%(asctime)s> %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO
+    )
+    try:
+        start_time = time.time()
+        kwargs = {key: arg for key, arg in kwargs.items() if arg is not None}
+        logging.info("Creating new AlphaTemplate with parameters:")
+        max_len = max(len(key) + 1 for key in kwargs)
+        for key, value in sorted(kwargs.items()):
+            logging.info(f"{key:<{max_len}} - {value}")
+        logging.info("")
+        import alphadia.dia
+        alphadia.dia.run_analysis(
+            dia_file_name=kwargs["dia_file_name"],
+            alphapept_library_file_name=kwargs["alphapept_library_file_name"],
+            output_file_name=kwargs["output_file_name"],
+            ppm=kwargs["ppm_tolerance"],
+            rt_tolerance=kwargs["rt_tolerance"],
+            mobility_tolerance=kwargs["mobility_tolerance"],
+            max_scan_difference=kwargs["max_scan_difference"],
+            max_cycle_difference=kwargs["max_cycle_difference"],
+            thread_count=kwargs["thread_count"],
+            fdr_rate=kwargs["fdr_rate"],
+        )
+    except Exception:
+        logging.exception("Something went wrong, execution incomplete!")
+    else:
+        logging.info(
+            f"Analysis done in {time.time() - start_time:.2f} seconds."
+        )
