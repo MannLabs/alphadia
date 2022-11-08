2bdec6eb044113166b2f96ad0be338035491a438
Sander Willems <sander.willems@bruker.com>
2022-11-08 10:40:41 +0100

    FEAT: quad calibration

--- alphadia/preprocessing/calibration.py
+++ alphadia/preprocessing/calibration.py
@@ -4,8 +4,8 @@
 import logging
 
 import numpy as np
-
 import alphatims.utils
+import numpy as np
 
 class QuadCalibrator:
 
@@ -125,3 +125,67 @@ def merge_cyclic_pushes(
             intensity = intensity_values[index]
             intensity_buffer[tof] += intensity
     return intensity_buffer
+
+
+class QuadWindowShape:
+
+    def __init__(
+        self,
+        file_name,#="/mnt/a54a8df1-78df-4788-bd29-6fca4115f5c0/software_development_data/synchroPASEF/quadCalibration.db",
+        quad_lines,#=slice(26, 30, 2),
+    ):
+        import sqlite3
+        import pandas as pd
+        with sqlite3.connect(
+            file_name
+        ) as sql_database_connection:
+            quad_cal_data = pd.read_sql_query(
+                "SELECT * from QuadCalibrationTable",
+                sql_database_connection
+            )
+            quad = quad_cal_data[quad_lines]
+        self.sp_mass = quad.SetpointMass.values
+        self.c1_params = get_slope_and_intercept_of_linear_function(
+            *self.sp_mass,
+            *quad.Center1.values
+        )
+        self.s1_params = get_slope_and_intercept_of_linear_function(
+            *self.sp_mass,
+            *quad.Sigma1.values
+        )
+        self.c2_params = get_slope_and_intercept_of_linear_function(
+            *self.sp_mass,
+            *quad.Center2.values
+        )
+        self.s2_params = get_slope_and_intercept_of_linear_function(
+            *self.sp_mass,
+            *quad.Sigma2.values
+        )
+
+
+    def get_efficiency_matrix(self, dia_data):
+        import math
+        cycle_center = np.mean(dia_data.cycle, axis=-1)
+        c1_params = self.c1_params
+        s1_params = self.s1_params
+        c2_params = self.c2_params
+        s2_params = self.s2_params
+        def get_transmission_matrix(mz):
+            center1 = mz * c1_params[0] + c1_params[1]
+            sigma1 = mz * s1_params[0] + s1_params[1]
+            center2 = mz * c2_params[0] + c2_params[1]
+            sigma2 = mz * s2_params[0] + s2_params[1]
+            out = (mz - cycle_center).copy().ravel()
+            for i, xi in enumerate(out):
+                arg1 = (xi - center1) / sigma1
+                arg2 = (center2  - xi) / sigma2
+                out[i] = 0.5 * (math.erf(arg1) + math.erf(arg2))
+            return out.reshape(cycle_center.shape)
+        return alphatims.utils.njit(get_transmission_matrix)
+
+
+@alphatims.utils.njit
+def get_slope_and_intercept_of_linear_function(x0, x1, y0, y1):
+    slope = (y1 - y0) / (x1 - x0)
+    intercept = y0 - x0 * slope
+    return slope, intercept
