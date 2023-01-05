373a05869f9b745447b93278159455bf9e45cf55
GeorgWa <gewaelbe@googlemail.com>
2023-01-05 18:14:51 +0100

    restrict unit tests to extraction

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -1,3 +1,3 @@
-python -m unittest unit_tests/test_cli.py
-python -m unittest unit_tests/test_gui.py
+#python -m unittest unit_tests/test_cli.py
+#python -m unittest unit_tests/test_gui.py
 python -m unittest unit_tests/test_extraction.py
