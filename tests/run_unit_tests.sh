15db7f35e25e2cdeda58db06d771af5c2cfae8a0
GeorgWa <gewaelbe@googlemail.com>
2023-08-25 14:35:02 +0200

    FEAT Add MS1 astral switch to gui

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -2,5 +2,5 @@ conda activate alphadia
 #python -m unittest unit_tests/test_cli.py
 #python -m unittest unit_tests/test_gui.py
 coverage run --source=../alphadia/extraction -m pytest
-# coverage-badge -f -o ../coverage.svg
+# coverage run --source=../alphadia/extraction -m pytest && coverage html && coverage-badge -f -o ../coverage.svg
 conda deactivate
