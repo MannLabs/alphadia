f38ebdffbead879bbcff81ff14f8e29691d876b5
GeorgWa <gewaelbe@googlemail.com>
2023-11-15 22:14:05 +0100

    CHORE updated unit tests

--- tests/unit_tests/test_workflow.py
+++ tests/unit_tests/test_workflow.py
@@ -276,8 +276,9 @@ def test_workflow_base():
             workflow_name = Path(file).stem
 
             my_workflow = base.WorkflowBase(
-                workflow_name, config, file, pd.DataFrame({})
+                workflow_name, config,
             )
+            my_workflow.load( file, pd.DataFrame({}))
 
             assert my_workflow.config["output"] == config["output"]
             assert my_workflow.instance_name == workflow_name
