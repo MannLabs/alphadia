454319658e129f26b8056c196c97894db1c71d81
GeorgWa <gewaelbe@googlemail.com>
2023-12-26 10:44:50 +0100

    include slow tests

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -1,7 +1,7 @@
 conda activate alphadia
 #python -m unittest unit_tests/test_cli.py
 #python -m unittest unit_tests/test_gui.py
-coverage run --source=../alphadia -m pytest -k 'not slow'
+coverage run --source=../alphadia -m pytest
 # coverage run --source=../alphadia -m pytest && coverage html && coverage-badge -f -o ../coverage.svg
 # coverage run --source=../alphadia -m pytest -k 'not slow' && coverage html && coverage-badge -f -o ../coverage.svg
 conda deactivate
