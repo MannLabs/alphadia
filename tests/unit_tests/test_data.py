1efdc021cf2de1addc5da15e31c2dc38555bbdc4
GeorgWa <gewaelbe@googlemail.com>
2024-01-17 15:28:58 +0100

    FEAT improved DIA cycle determination

--- tests/unit_tests/test_data.py
+++ tests/unit_tests/test_data.py
@@ -21,6 +21,24 @@ def test_transpose():
     assert np.allclose(tof_indptr, _tof_indptr)
     assert np.allclose(intensity_values, _intensity_values)
 
+def test_cycle():
+    
+    rand_cycle_start = np.random.randint(0, 100)
+    rand_cycle_length = np.random.randint(5, 10)
+    rand_num_cycles = np.random.randint(10, 50)
+
+
+    cycle = np.zeros(rand_cycle_start)
+    cycle = np.append(cycle, np.tile(np.arange(rand_cycle_length), rand_num_cycles))
+
+    cycle_length = thermo.get_cycle_length(cycle)
+    cycle_start = thermo.get_cycle_start(cycle, cycle_length)
+    cycle_valid = thermo.assert_cycle(cycle, cycle_length, cycle_start)
+
+    assert cycle_valid
+    assert cycle_length == rand_cycle_length
+    assert cycle_start == rand_cycle_start
+
 
 @pytest.mark.slow
 def test_raw_data():
