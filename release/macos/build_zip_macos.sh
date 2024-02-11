76da35fd9af23e7200710ce366f6a97f5f25932e
GeorgWa <gewaelbe@googlemail.com>
2024-02-11 22:26:50 +0100

    updated build script

--- release/macos/build_zip_macos.sh
+++ release/macos/build_zip_macos.sh
@@ -6,7 +6,7 @@ PACKAGE_VERSION="1.5.3"
 
 ARCH=$(uname -m)
 if [ "$ARCH" == "x86_64" ]; then
-  ARCH="amd64"
+  ARCH="x64"
 fi
 KERNEL=$(uname -s | tr '[:upper:]' '[:lower:]')
 
@@ -18,13 +18,34 @@ rm -rf ../../dist/${BUILD_NAME}
 # Create the dist folder
 mkdir -p ../../dist/${BUILD_NAME}
 
-ll ../../gui/out/alphadia-gui-${KERNEL}-${ARCH}
+# === GUI ===
+
+ls ../../gui/out
+
+# Set the path to the GUI build
+GUI_BUILD="../../gui/out/alphadia-gui-${KERNEL}-${ARCH}"
+
+# Check if the GUI build exists, otherwise exit with an error
+if [ ! -d "$GUI_BUILD" ]; then
+  echo "GUI build not found at $GUI_BUILD"
+  exit 1
+fi
 
 # Copy the electron forge build
-cp -a ../../gui/out/alphadia-gui-${KERNEL}-${ARCH}/. ../../dist/${BUILD_NAME}
+cp -a ${GUI_BUILD}/. ../../dist/${BUILD_NAME}
+
+# === Backend ===
+
+BACKEND_BUILD="../../dist/alphadia"
+
+# Check if the backend build exists, otherwise exit with an error
+if [ ! -d "$BACKEND_BUILD" ]; then
+  echo "Backend build not found at $BACKEND_BUILD"
+  exit 1
+fi
 
 # Copy the backend
-cp -a ../../dist/alphadia/. ../../dist/${BUILD_NAME}
+cp -a {BACKEND_BUILD}/. ../../dist/${BUILD_NAME}
 
 # create the zip file
 cd ../../dist
