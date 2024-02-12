1405f5220925dc390b3536c5319a87a5a6e76a25
GeorgWa <gewaelbe@googlemail.com>
2024-02-12 22:08:59 +0100

    full workflow

--- release/windows/build_zip_windows.sh
+++ release/windows/build_zip_windows.sh
@@ -4,7 +4,7 @@
 PACKAGE_NAME="alphadia"
 PACKAGE_VERSION="1.5.3"
 
-BUILD_NAME="${PACKAGE_NAME}-${PACKAGE_VERSION}-win-x64"
+BUILD_NAME="${PACKAGE_NAME}-${PACKAGE_VERSION}-win32-x64"
 
 # Cleanup the dist folder
 rm -rf dist/${BUILD_NAME}
@@ -14,19 +14,19 @@ mkdir -p dist/${BUILD_NAME}
 
 # === GUI ===
 
-#ls gui/out
+ls gui/out
 
 # Set the path to the GUI build
-#GUI_BUILD="gui/out/alphadia-gui-${KERNEL}-${ARCH}"
+GUI_BUILD="gui/out/alphadia-gui-${KERNEL}-${ARCH}"
 
 # Check if the GUI build exists, otherwise exit with an error
-#if [ ! -d "$GUI_BUILD" ]; then
-#  echo "GUI build not found at $GUI_BUILD"
-#  exit 1
-#fi
+if [ ! -d "$GUI_BUILD" ]; then
+  echo "GUI build not found at $GUI_BUILD"
+  exit 1
+fi
 
 # Copy the electron forge build
-#cp -a ${GUI_BUILD}/. dist/${BUILD_NAME}
+cp -a ${GUI_BUILD}/. dist/${BUILD_NAME}
 
 # === Backend ===
 
