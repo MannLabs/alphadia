f147ae6eeaef5f2fb548174a89c19a3303bbc711
GeorgWa <wallmann@biochem.mpg.de>
2024-02-26 11:55:03 +0100

    FIX remove debugging statement

--- alphadia/workflow/peptidecentric.py
+++ alphadia/workflow/peptidecentric.py
@@ -774,7 +774,7 @@ class PeptideCentricWorkflow(base.WorkflowBase):
             apply_cutoff=True,
         )
 
-        features_df.to_csv("/Users/georgwallmann/Library/CloudStorage/OneDrive-Personal/Studium/MPI/AG Mann/alphaDIA/2024_02_25_feature_importance/features.tsv", sep="\t")
+        #features_df.to_csv("/Users/georgwallmann/Library/CloudStorage/OneDrive-Personal/Studium/MPI/AG Mann/alphaDIA/2024_02_25_feature_importance/features.tsv", sep="\t")
         
         precursor_df = self.fdr_correction(features_df, fragments_df)
         
