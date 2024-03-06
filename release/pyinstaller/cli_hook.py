daf6c0b783ccc478c5170fadd0be4edf36c07d95
GeorgWa <wallmann@biochem.mpg.de>
2024-03-06 16:25:23 +0100

    trigger

--- release/pyinstaller/cli_hook.py
+++ release/pyinstaller/cli_hook.py
@@ -8,12 +8,16 @@ if __name__ == "__main__":
     # catch pyinstaller bug
     # subprocess.CalledProcessError: Command '['/Applications/alphaDIA.app/Contents/Frameworks/alphadia', '-sS', '-c', 'import platform; print(platform.mac_ver()[0])']' returned non-zero exit status 2.
     # [36481] Failed to execute script 'cli_hook' due to unhandled exception!
+    print(sys.argv)
+    
+    for i, arg in enumerate(sys.argv):
+        print(i, arg)
+
     if sys.argv[1] == "-sS" & sys.argv[2] == "-c":
         import platform
 
         print(platform.mac_ver()[0])
         sys.exit(0)
         
-
     multiprocessing.freeze_support()
     cli.run()
