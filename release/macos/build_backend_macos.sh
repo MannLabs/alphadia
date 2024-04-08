c545e6622352a5fbbea6c69e0078a794e690f58a
GeorgWa <wallmann@biochem.mpg.de>
2024-04-08 14:08:42 +0200

    Bump version: 1.5.4 → 1.5.5

--- release/macos/build_backend_macos.sh
+++ release/macos/build_backend_macos.sh
@@ -3,7 +3,7 @@
 # Creating the wheel
 # Creating the wheel
 python setup.py sdist bdist_wheel
-pip install "dist/alphadia-1.5.4-py3-none-any.whl[stable]"
+pip install "dist/alphadia-1.5.5-py3-none-any.whl[stable]"
 
 # Creating the stand-alone pyinstaller folder
 pip install pyinstaller
