8bcdebcbb388081567d7f9b0dd64dfe6a73a96f7
GeorgWa <wallmann@biochem.mpg.de>
2024-04-17 15:31:26 +0200

    add pyinstaller

--- release/macos/build_backend_macos.sh
+++ release/macos/build_backend_macos.sh
@@ -9,4 +9,5 @@ python setup.py sdist bdist_wheel
 pip install "dist/alphadia-1.5.5-py3-none-any.whl[stable]"
 
 # Creating the stand-alone pyinstaller folder
+pip install pyinstaller
 pyinstaller release/pyinstaller/alphadia.spec -y
