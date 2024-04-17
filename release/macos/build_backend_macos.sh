4eccd48f61d1141f90200755a1c5ba47d5e6a1ad
GeorgWa <wallmann@biochem.mpg.de>
2024-04-17 15:22:36 +0200

    use hf bugfix

--- release/macos/build_backend_macos.sh
+++ release/macos/build_backend_macos.sh
@@ -2,13 +2,11 @@
 
 # Creating the wheel
 # Creating the wheel
-pip install huggingface_hub
+pip git+install https://github.com/huggingface/huggingface_hub.git
 python -c 'from huggingface_hub import get_full_repo_name; print("success")'
 
 python setup.py sdist bdist_wheel
 pip install "dist/alphadia-1.5.5-py3-none-any.whl[stable]"
 
 # Creating the stand-alone pyinstaller folder
-pip install pyinstaller
-python -c 'from huggingface_hub import get_full_repo_name; print("success")'
 pyinstaller release/pyinstaller/alphadia.spec -y
