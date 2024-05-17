8d372f7699b56654d7f98e80f66279e2e550329d
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-05-17 15:50:29 +0200

    #140: trying self-hosted runner

--- tests/run_e2e_tests.sh
+++ tests/run_e2e_tests.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e -u
 TEST_CASE_NAME=$1
+ENV_NAME=$2
 
 cd e2e_tests
 
-python prepare_test_data.py $TEST_CASE_NAME
+conda run -n $ENV_NAME python prepare_test_data.py $TEST_CASE_NAME
 ls */*
 
 cat $TEST_CASE_NAME/config.yaml
 
-alphadia --config $TEST_CASE_NAME/config.yaml
+conda run -n $ENV_NAME alphadia --config $TEST_CASE_NAME/config.yaml
 ls */*
 
-python calc_metrics.py $TEST_CASE_NAME
+conda run -n $ENV_NAME python calc_metrics.py $TEST_CASE_NAME
 
 cd -
