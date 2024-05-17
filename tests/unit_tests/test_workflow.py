4f08e4f4fe6ec564c019049194c7fdbabb4e4592
GeorgWa <wallmann@biochem.mpg.de>
2024-05-17 11:09:14 +0200

    fix test

--- tests/unit_tests/test_workflow.py
+++ tests/unit_tests/test_workflow.py
@@ -7,7 +7,7 @@ import yaml
 import pytest
 from pathlib import Path
 
-from alphadia.calibration.provider import Calibration
+from alphadia.calibration.property import Calibration
 from sklearn.linear_model import LinearRegression
 from alphadia.calibration.models import LOESSRegression
 from alphadia.workflow import manager, base
