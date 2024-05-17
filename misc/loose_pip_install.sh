7fd6e2287647aa3861fcaef6dc51365e6f4a949e
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-05-17 15:05:27 +0200

    #140: trying self-hosted runner

--- misc/loose_pip_install.sh
+++ misc/loose_pip_install.sh
@@ -1,9 +1,11 @@
-conda create -n alphadia python=3.9 -y
-conda info --envs
-conda init bash
-source /home/alphadia/miniconda3/etc/profile.d/conda.sh
+ENV_NAME=${1:-alphadia}
 
-conda activate alphadia
+conda create -n $ENV_NAME python=3.9 -y
+#conda info --envs
+#conda init bash
+#source /home/alphadia/miniconda3/etc/profile.d/conda.sh
+
+conda activate $ENV_NAME
 pip install -e '../.[development]'
 alphadia -v
 conda deactivate
