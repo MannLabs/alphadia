1f17f95caacdf38c3a69bf4a9f7194b22c78a850
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-05-17 13:02:03 +0200

    #140: trying self-hosted runner

--- misc/loose_pip_install.sh
+++ misc/loose_pip_install.sh
@@ -1,6 +1,8 @@
 conda create -n alphadia python=3.9 -y
 conda info --envs
 conda init bash
+source /home/alphadia/miniconda3/etc/profile.d/conda.sh
+
 conda activate alphadia
 pip install -e '../.[development]'
 alphadia -v
