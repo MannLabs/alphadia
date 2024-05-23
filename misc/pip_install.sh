53fcc05c371d6b795f56bf24747b796bb8baad13
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-05-23 13:24:54 +0200

    #169: move test requirements to dedicated file, run stable version in tests, refactor pip_install.sh

--- misc/pip_install.sh
+++ misc/pip_install.sh
@@ -1,7 +1,10 @@
-ENV_NAME=${1:-alphadia}
+set -e -u
+
+INSTALL_TYPE=$1 # stable, loose, etc..
+ENV_NAME=${2:-alphadia}
 
 conda create -n $ENV_NAME python=3.9 -y
 
 # conda 'run' vs. 'activate', cf. https://stackoverflow.com/a/72395091
-conda run -n $ENV_NAME --no-capture-output pip install -e '../.[loose]'
+conda run -n $ENV_NAME --no-capture-output pip install -e "../.[${INSTALL_TYPE}]"
 conda run -n $ENV_NAME --no-capture-output alphadia -v
--- misc/stable_pip_install.sh
+++
@@ -1,3 +0,0 @@
-conda create -n alphadia python=3.9 -y
-conda run -n alphadia --no-capture-output pip install -e '../.[stable]'
-conda run -n alphadia --no-capture-output alphadia -v
