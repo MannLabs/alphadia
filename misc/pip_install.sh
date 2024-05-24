846048e937c422ade2c8b4016a413b44be34564f
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-05-24 17:27:06 +0200

    #169: adapt pip install script to new handling of dependencies

--- misc/pip_install.sh
+++ misc/pip_install.sh
@@ -5,6 +5,12 @@ ENV_NAME=${2:-alphadia}
 
 conda create -n $ENV_NAME python=3.9 -y
 
+if [ "$INSTALL_TYPE" = "loose" ]; then
+  INSTALL_STRING=""
+else
+  INSTALL_STRING="[${INSTALL_TYPE}]"
+fi
+
 # conda 'run' vs. 'activate', cf. https://stackoverflow.com/a/72395091
-conda run -n $ENV_NAME --no-capture-output pip install -e "../.[${INSTALL_TYPE}]"
+conda run -n $ENV_NAME --no-capture-output pip install -e "../.$INSTALL_STRING"
 conda run -n $ENV_NAME --no-capture-output alphadia -v
