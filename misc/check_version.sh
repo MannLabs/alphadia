fca70053b4b4ec825f1cbf73caa919e62724a1dd
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-05-28 11:28:13 +0200

    #174: improve check_version.sh

--- misc/check_version.sh
+++ misc/check_version.sh
@@ -1,11 +1,16 @@
-current_version=$(grep "__version__" ../alphadia/__init__.py | cut -f3 -d ' ' | sed 's/"//g')
+current_version=$(grep "__version__" ../alpakaweird/__init__.py | cut -f3 -d ' ' | sed 's/"//g')
 current_version_as_regex=$(echo $current_version | sed 's/\./\\./g')
-conda create -n version_check python=3.8 pip=20.1 -y
-conda activate version_check
+echo current_version_as_regex=$current_version_as_regex
+
 set +e
-already_on_pypi=$(pip install alphadia== 2>&1 | grep -c "$current_version_as_regex")
+conda create -n version_check python=3.9 -y
+pip_output=$(conda run -n version_check pip install --index-url https://test.pypi.org/simple/  alpakaweird 2>&1)
+echo $pip_output=$pip_output
+
+already_on_pypi=$(echo $pip_output | grep -c alpakaweird-"$current_version_as_regex")
+echo $already_on_pypi=$already_on_pypi
 set -e
-conda deactivate
+
 if [ $already_on_pypi -ne 0 ]; then
   echo "Version is already on PyPi"
   exit 1
