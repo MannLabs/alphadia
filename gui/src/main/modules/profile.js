24c4ba748f0ff3f8e75d5b55bc2b7650e9f7b064
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-05-29 17:26:01 +0200

    bump version to 1.6.1

--- gui/src/main/modules/profile.js
+++ gui/src/main/modules/profile.js
@@ -7,7 +7,7 @@ const { dialog } = require('electron')
 const Profile = class {
 
     config = {
-        "version": "1.6.0",
+        "version": "1.6.1",
         "conda": {
             "envName": "alpha",
             "path": ""
