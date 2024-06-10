fccc463374ee10df83abb00cdb42b9e6ca9f4db7
GeorgWa <wallmann@biochem.mpg.de>
2024-06-10 15:04:51 -0400

    pip no cache

--- misc/pip_install.sh
+++ misc/pip_install.sh
@@ -3,7 +3,7 @@ set -e -u
 INSTALL_TYPE=$1 # stable, loose, etc..
 ENV_NAME=${2:-alphadia}
 
-conda create -n $ENV_NAME python=3.9 -y
+conda create -n $ENV_NAME python=3.11 -y
 
 if [ "$INSTALL_TYPE" = "loose" ]; then
   INSTALL_STRING=""
@@ -12,5 +12,5 @@ else
 fi
 
 # conda 'run' vs. 'activate', cf. https://stackoverflow.com/a/72395091
-conda run -n $ENV_NAME --no-capture-output pip install -e "../.$INSTALL_STRING"
+conda run -n $ENV_NAME --no-capture-output pip install --no-cache-dir -e "../.$INSTALL_STRING"
 conda run -n $ENV_NAME --no-capture-output alphadia -v
