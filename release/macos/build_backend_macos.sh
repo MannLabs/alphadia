4272ae646f8af9f6b725536c26832572fd36a8b7
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-09-04 16:56:14 +0200

    get rid of most pip installs in installer building

--- release/macos/build_backend_macos.sh
+++ release/macos/build_backend_macos.sh
@@ -2,10 +2,8 @@
 
 # navigate to the root directory
 
-pip install build
 python -m build
 pip install "dist/alphadia-1.7.2-py3-none-any.whl[stable]"
 
 # Creating the stand-alone pyinstaller folder
-pip install pyinstaller
 pyinstaller release/pyinstaller/alphadia.spec -y
