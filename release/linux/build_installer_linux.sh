b3d819712ec749cb9efddf5aa575357c865ee3e6
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-09-10 12:50:22 +0200

    limit linux installer building to cpu

--- release/linux/build_installer_linux.sh
+++ release/linux/build_installer_linux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e -u
 
+CPU_OR_GPU=${1:-CPU}
+
 # Build the installer for Linux.
 # This script must be run from the root of the repository.
 
@@ -9,6 +11,10 @@ rm -rf dist_pyinstaller build_pyinstaller
 
 python -m build
 pip install "dist/alphadia-1.7.2-py3-none-any.whl[stable]"
+
+if [ "${CPU_OR_GPU}" != "GPU" ]; then
+    pip install torch -U --extra-index-url https://download.pytorch.org/whl/cpu
+fi
 
 # Creating the stand-alone pyinstaller folder
 pyinstaller release/pyinstaller/alphadia.spec --distpath dist_pyinstaller --workpath build_pyinstaller -y
