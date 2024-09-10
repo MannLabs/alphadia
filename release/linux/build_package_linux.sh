5b4c4b66e8620f1ecb650d9fbdca180c3874e4ed
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-09-10 09:35:12 +0200

    add gui to linux package

--- release/linux/build_package_linux.sh
+++ release/linux/build_package_linux.sh
@@ -9,9 +9,31 @@ PACKAGE_NAME=alphadia
 # BUILD_NAME is taken from environment variables, e.g. 'alphadia-1.2.1-linux-x64'
 rm -rf ${BUILD_NAME}.deb
 
+mkdir -p dist_pyinstaller/${BUILD_NAME}/usr/local/bin
+
+# === GUI ===
+echo "Copying GUI"
+
+# TODO find out where this contract from ARCH & KERNEL is used in GUI building
+ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
+  ARCH="x64"
+fi
+KERNEL=$(uname -s | tr '[:upper:]' '[:lower:]')
+echo ARCH=${ARCH} KERNEL=${KERNEL}
+
+ls ./gui/out
+
+GUI_BUILD="./gui/out/${PACKAGE_NAME}-gui-${KERNEL}-${ARCH}"
+if [ ! -d "$GUI_BUILD" ]; then
+  echo "GUI build not found at $GUI_BUILD"
+  exit 1
+fi
+
+cp -a ${GUI_BUILD}/. dist_pyinstaller/${BUILD_NAME}/usr/local/bin/
+
 
 # Wrapping the pyinstaller folder in a .deb package
-mkdir -p dist_pyinstaller/${BUILD_NAME}/usr/local/bin
 mv dist_pyinstaller/${PACKAGE_NAME} dist_pyinstaller/${BUILD_NAME}/usr/local/bin/${PACKAGE_NAME}
 mkdir dist_pyinstaller/${BUILD_NAME}/DEBIAN
 cp release/linux/control dist_pyinstaller/${BUILD_NAME}/DEBIAN
