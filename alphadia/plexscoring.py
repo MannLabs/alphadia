6ea77a2740cd94a2db97cad89c703671f4469e92
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-11-04 13:08:23 +0100

    remove copy of pjit.py

--- alphadia/plexscoring.py
+++ alphadia/plexscoring.py
@@ -13,7 +13,6 @@ import pandas as pd
 from alphadia import features, quadrupole, utils, validate
 from alphadia.data import alpharaw_wrapper, bruker
 from alphadia.numba import config, fragments
-from alphadia.pjit import pjit
 from alphadia.plotting.cycle import plot_cycle
 from alphadia.plotting.debug import (
     plot_fragment_profile,
@@ -1372,7 +1371,7 @@ class OuptutPsmDF:
         )
 
 
-@pjit
+@alphatims.utils.pjit
 def _executor(
     i,
     sg_container,
@@ -1398,7 +1397,7 @@ def _executor(
     )
 
 
-@pjit
+@alphatims.utils.pjit
 def transfer_feature(
     idx, score_group_container, feature_array, precursor_idx_array, rank_array
 ):
