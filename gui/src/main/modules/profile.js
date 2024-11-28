149e7faea867a0abae569ab7c051c41d82c08bc2
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-11-28 09:37:43 +0100

    Bump version: 1.9.0 → 1.9.1

--- gui/src/main/modules/profile.js
+++ gui/src/main/modules/profile.js
@@ -3,7 +3,7 @@ const path = require("path")
 const { app, shell, BrowserWindow} = require("electron")
 const { dialog } = require('electron')
 
-const VERSION = "1.9.0"
+const VERSION = "1.9.1"
 
 const Profile = class {
 
