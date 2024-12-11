adc94e12f29ed4ea4f6c1eed0f5d1ccab04cdddb
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-12-11 16:29:20 +0100

    refactoring

--- alphadia/search_plan.py
+++ alphadia/search_plan.py
@@ -1,6 +1,7 @@
 """Search plan for multistep search."""
 
 import os
+from pathlib import Path
 
 import yaml
 
@@ -8,9 +9,9 @@ from alphadia.outputtransform import SearchPlanOutput
 from alphadia.planning import SPECLIB_FILE_NAME, Plan, logger
 from alphadia.workflow.base import QUANT_FOLDER_NAME
 
-STEP1_NAME = "transfer"
-STEP2_NAME = "library"
-STEP3_NAME = "mbr"
+TRANSFER_STEP_NAME = "transfer"
+LIBRARY_STEP_NAME = "library"
+MBR_STEP_NAME = "mbr"
 
 
 class SearchPlan:
@@ -44,119 +45,141 @@ class SearchPlan:
         """
 
         self._user_config = config
-        self.output_directory = output_directory
-        self.library_path = library_path
-        self.fasta_path_list = fasta_path_list
-        self.quant_dir = quant_dir
-        self.raw_path_list = raw_path_list
+        self._output_directory = Path(output_directory)
+        self._library_path = Path(library_path)
+        self._fasta_path_list = fasta_path_list
+        self._quant_dir = Path(quant_dir)
+        self._raw_path_list = raw_path_list
+
+        # these are the default settings if the library step is the only one
+        self._library_step_quant_dir = self._quant_dir
+        self._library_step_library_path = self._library_path
+        self._library_step_output_dir = self._output_directory
+
+        # multistep search:
+        self._multistep_config = None
+        self._transfer_step_output_dir = None
+        self._mbr_step_quant_dir = None
+        self._mbr_step_library_path = None
 
         multistep_search_config = self._user_config.get("multistep_search", {})
-        self.step1_enabled = multistep_search_config.get("transfer_step_enabled", False)
-        self.step3_enabled = multistep_search_config.get("mbr_step_enabled", False)
+        self._transfer_step_enabled = multistep_search_config.get(
+            "transfer_step_enabled", False
+        )
+        self._mbr_step_enabled = multistep_search_config.get("mbr_step_enabled", False)
 
-        with open(
-            os.path.join(os.path.dirname(__file__), "constants", "multistep.yaml")
-        ) as f:
-            self.multistep_config = yaml.safe_load(f)
+        if self._transfer_step_enabled or self._mbr_step_enabled:
+            self._update_paths()
+            with Path(
+                os.path.dirname(__file__) / "constants" / "multistep.yaml"
+            ).open() as f:
+                self._multistep_config = yaml.safe_load(f)
 
-        self.step2_name = None
-        self.step2_quant_dir = self.quant_dir
-        self.step2_library_path = self.library_path
-        self.step2_output_dir = self.output_directory
+    def _update_paths(self) -> None:
+        """Set directories for the different steps.
 
-        self.step1_output_dir = None
-        if self.step1_enabled:
-            self.step1_output_dir = os.path.join(self.output_directory, STEP1_NAME)
+        If the transfer step is enabled, the quant and library paths for the library step are pointed to the
+            output of the transfer step.
+        If the mbr step is enabled, the quant and library paths for the mbr step are pointed to the output of the
+            library step. Also, the output path for the library step is adjusted to be in a subdirectory of the original output path.
+        """
 
-            self.step2_quant_dir = os.path.join(
-                self.step1_output_dir, QUANT_FOLDER_NAME
+        # in case transfer step is enabled, we need to adjust the library step settings
+        if self._transfer_step_enabled:
+            self._transfer_step_output_dir = self._output_directory / TRANSFER_STEP_NAME
+            self._library_step_quant_dir = (
+                self._transfer_step_output_dir / QUANT_FOLDER_NAME
             )
-            self.step2_library_path = os.path.join(
-                self.step1_output_dir, SPECLIB_FILE_NAME
+            self._library_step_library_path = (
+                self._transfer_step_output_dir / SPECLIB_FILE_NAME
             )
-            self.step2_output_dir = os.path.join(self.output_directory, STEP2_NAME)
-            self.step2_name = STEP2_NAME
 
-        self.step3_quant_dir = None
-        self.step3_library_path = None
-        self.step3_output_dir = None
-        if self.step3_enabled:
-            self.step3_quant_dir = os.path.join(
-                self.step2_output_dir, QUANT_FOLDER_NAME
+        # in case mbr step is enabled, we need to adjust the library step settings
+        if self._mbr_step_enabled:
+            self._mbr_step_quant_dir = self._library_step_output_dir / QUANT_FOLDER_NAME
+            self._mbr_step_library_path = (
+                self._library_step_output_dir / SPECLIB_FILE_NAME
             )
-            self.step3_library_path = os.path.join(
-                self.step2_output_dir, SPECLIB_FILE_NAME
-            )
-            self.step3_output_dir = self.output_directory
+            self._library_step_output_dir = self._output_directory / LIBRARY_STEP_NAME
 
     def run_plan(self):
-        """Run the search plans."""
+        """Run the search plan.
 
-        step2_extra_config = {}
-        if self.step1_enabled:
-            # predict library (once for all files, file-independent), search all files (emb. parallel), quantify all files together (combine all files)
-            # (outer.sh-steps 1, 2, 3)
+        Depending on what steps are to be run, the relevant information (e.g. file paths or thresholds) is passed
+        from one to the next step via 'extra config'.
+        """
+
+        library_step_extra_config = {}
+        if self._transfer_step_enabled:
+            # predict library (once for all files, file-independent), search all files (emb. parallel), quantify all files together (combine all files) (outer.sh-steps 1, 2, 3)
             # output: DL model
             self.run_step(
-                self.step1_output_dir,
-                self.library_path,
-                self.multistep_config[STEP1_NAME],
-                self.quant_dir,
+                self._transfer_step_output_dir,
+                self._library_path,
+                self._multistep_config[TRANSFER_STEP_NAME],
+                self._quant_dir,
             )
 
             add_config = {
                 "library_prediction": {
                     "peptdeep_model_path": os.path.join(
-                        self.step1_output_dir, SearchPlanOutput.TRANSFER_MODEL
+                        self._transfer_step_output_dir, SearchPlanOutput.TRANSFER_MODEL
                     )
                 }
             }
-            step2_extra_config = self.multistep_config[STEP2_NAME] | add_config
+            library_step_extra_config = (
+                self._multistep_config[LIBRARY_STEP_NAME] | add_config
+            )
 
-        # same as step1
+        # same as transfer_step
         # output: MBR library
         library_plan = self.run_step(
-            self.step2_output_dir,
-            self.step2_library_path,
-            step2_extra_config,
-            self.step2_quant_dir,
+            self._library_step_output_dir,
+            self._library_step_library_path,
+            library_step_extra_config,
+            self._library_step_quant_dir,
         )
 
-        if self.step3_enabled and library_plan is not None:
+        if self._mbr_step_enabled:
             # (outer.sh-steps 4,5)
             add_config = self._update_config_from_library_plan(library_plan)
-            extra_config = self.multistep_config[STEP2_NAME] | add_config
+            mbr_step_extra_config = (
+                self._multistep_config[LIBRARY_STEP_NAME] | add_config
+            )
             self.run_step(
-                self.step3_output_dir,
-                self.step3_library_path,
-                extra_config,
-                self.step3_quant_dir,
+                self._output_directory,
+                self._mbr_step_library_path,
+                mbr_step_extra_config,
+                self._mbr_step_quant_dir,
             )
 
     def run_step(
         self,
-        output_directory: str,
-        library_path: str,
+        output_directory: Path,
+        library_path: Path,
         extra_config: dict,
-        quant_dir: str | None,
+        quant_dir: Path | None,
     ) -> Plan:
+        """Run a single step of the search plan."""
         plan = Plan(
-            output_directory,
-            raw_path_list=self.raw_path_list,
-            library_path=library_path,
-            fasta_path_list=self.fasta_path_list,
+            str(output_directory),
+            raw_path_list=self._raw_path_list,
+            library_path=str(library_path),
+            fasta_path_list=self._fasta_path_list,
             config=self._user_config,
             extra_config=extra_config,
-            quant_path=quant_dir,
+            quant_path=None if quant_dir is None else str(quant_dir),
         )
         plan.run()
         return plan
 
-    def _update_config_from_library_plan(self, library_plan):
+    def _update_config_from_library_plan(self, library_plan: Plan) -> dict:
+        """Update the config based on the library plan."""
+        new_config = {}
         # take any required information from library_plan and pass it via config to the next step, e.g.
-        new_config = self._user_config | {  # noqa: F841
-            "search": {"target_ms1_tolerance": library_plan.estimators["ms1_accuracy"]}
-        }
-        # think about hardcoding everything here
+        # new_config = self._user_config | {  # noqa: F841
+        #     "search": {"target_ms1_tolerance": library_plan.estimators["ms1_accuracy"]}
+        # }
+
         logger.info(f"Using ms1_accuracy: {library_plan.estimators['ms1_accuracy']}")
         return new_config
