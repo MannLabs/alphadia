b60050110e3047b096caf2044ecee1a293ba9ddb
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-12-12 14:02:21 +0100

    make tests easier to debug

--- tests/unit_tests/test_search_plan.py
+++ tests/unit_tests/test_search_plan.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, call, patch
+from unittest.mock import MagicMock, patch
 
 from alphadia.planning import Plan
 from alphadia.search_plan import SearchPlan
@@ -36,15 +36,16 @@ def test_runs_plan_without_transfer_and_mbr_steps(mock_plan, mock_init_logging):
 
     mock_init_logging.assert_called_once_with("/user_provided_output_path")
 
-    mock_plan.assert_called_once_with(
-        output_folder="/user_provided_output_path",
-        raw_path_list=["/raw1"],
-        library_path="/user_provided_library_path",
-        fasta_path_list=["/fasta1"],
-        config={"some_user_config_key": "some_user_config_value"},
-        extra_config={},
-        quant_path="/user_provided_quant_path",
-    )
+    # could use `mock_plan.assert_has_calls([call(..)])` pattern here but it is harder to read in case of error
+    assert mock_plan.call_args_list[0].kwargs == {
+        "output_folder": "/user_provided_output_path",
+        "raw_path_list": ["/raw1"],
+        "library_path": "/user_provided_library_path",
+        "fasta_path_list": ["/fasta1"],
+        "config": {"some_user_config_key": "some_user_config_value"},
+        "extra_config": {},
+        "quant_path": "/user_provided_quant_path",
+    }
 
     mock_plan.return_value.run.assert_called_once_with()
 
@@ -76,37 +77,31 @@ def test_runs_plan_with_transfer_step(
 
     mock_init_logging.assert_called_once_with("/user_provided_output_path")
 
-    mock_plan.assert_has_calls(
-        [
-            call(
-                output_folder="/user_provided_output_path/transfer",
-                raw_path_list=["/raw1"],
-                library_path="/user_provided_library_path",
-                fasta_path_list=["/fasta1"],
-                config=user_config | multistep_search_config,
-                extra_config=MOCK_MULTISTEP_CONFIG["transfer"],
-                quant_path="/user_provided_quant_path",
-            ),
-            call().run(),
-            call(
-                output_folder="/user_provided_output_path",
-                raw_path_list=["/raw1"],
-                library_path="/user_provided_library_path",
-                fasta_path_list=["/fasta1"],
-                config=user_config | multistep_search_config,
-                extra_config=MOCK_MULTISTEP_CONFIG["library"]
-                | {
-                    "library_prediction": {
-                        "peptdeep_model_path": "/user_provided_output_path/transfer/peptdeep.transfer",
-                        "predict": True,
-                    },
-                }
-                | dynamic_config,
-                quant_path="/user_provided_output_path/transfer/quant",
-            ),
-            call().run(),
-        ]
-    )
+    assert mock_plan.call_args_list[0].kwargs == {
+        "output_folder": "/user_provided_output_path/transfer",
+        "raw_path_list": ["/raw1"],
+        "library_path": "/user_provided_library_path",
+        "fasta_path_list": ["/fasta1"],
+        "config": user_config | multistep_search_config,
+        "extra_config": MOCK_MULTISTEP_CONFIG["transfer"],
+        "quant_path": "/user_provided_quant_path",
+    }
+    assert mock_plan.call_args_list[1].kwargs == {
+        "output_folder": "/user_provided_output_path",
+        "raw_path_list": ["/raw1"],
+        "library_path": "/user_provided_library_path",
+        "fasta_path_list": ["/fasta1"],
+        "config": user_config | multistep_search_config,
+        "extra_config": MOCK_MULTISTEP_CONFIG["library"]
+        | {
+            "library_prediction": {
+                "peptdeep_model_path": "/user_provided_output_path/transfer/peptdeep.transfer",
+                "predict": True,
+            },
+        }
+        | dynamic_config,
+        "quant_path": "/user_provided_output_path/transfer/quant",
+    }
 
 
 @patch("alphadia.search_plan.reporting.init_logging")
@@ -134,30 +129,25 @@ def test_runs_plan_with_mbr_step(mock_get_dyn_config, mock_plan, mock_init_loggi
 
     mock_init_logging.assert_called_once_with("/user_provided_output_path")
 
-    mock_plan.assert_has_calls(
-        [
-            call(
-                output_folder="/user_provided_output_path/library",
-                raw_path_list=["/raw1"],
-                library_path="/user_provided_library_path",
-                fasta_path_list=["/fasta1"],
-                config=user_config | multistep_search_config,
-                extra_config=MOCK_MULTISTEP_CONFIG["library"],
-                quant_path="/user_provided_quant_path",
-            ),
-            call().run(),
-            call(
-                output_folder="/user_provided_output_path",
-                raw_path_list=["/raw1"],
-                library_path="/user_provided_output_path/library/speclib.mbr.hdf",
-                fasta_path_list=["/fasta1"],
-                config=user_config | multistep_search_config,
-                extra_config=MOCK_MULTISTEP_CONFIG["mbr"] | dynamic_config,
-                quant_path="/user_provided_output_path/library/quant",
-            ),
-            call().run(),
-        ],
-    )
+    assert mock_plan.call_args_list[0].kwargs == {
+        "output_folder": "/user_provided_output_path/library",
+        "raw_path_list": ["/raw1"],
+        "library_path": "/user_provided_library_path",
+        "fasta_path_list": ["/fasta1"],
+        "config": user_config | multistep_search_config,
+        "extra_config": MOCK_MULTISTEP_CONFIG["library"],
+        "quant_path": "/user_provided_quant_path",
+    }
+
+    assert mock_plan.call_args_list[1].kwargs == {
+        "output_folder": "/user_provided_output_path",
+        "raw_path_list": ["/raw1"],
+        "library_path": "/user_provided_output_path/library/speclib.mbr.hdf",
+        "fasta_path_list": ["/fasta1"],
+        "config": user_config | multistep_search_config,
+        "extra_config": MOCK_MULTISTEP_CONFIG["mbr"] | dynamic_config,
+        "quant_path": "/user_provided_output_path/library/quant",
+    }
 
 
 @patch("alphadia.search_plan.reporting.init_logging")
@@ -186,11 +176,6 @@ def test_runs_plan_with_transfer_and_mbr_steps(
     search_plan.run_plan()
 
     mock_init_logging.assert_called_once_with("/user_provided_output_path")
-
-    # TODO add case with None quant_dir/lib
-    # TODO: make this nicer to debug
-
-    # could use mock_plan.assert_has_calls([call(..)]) pattern here but it is harder to read in case of error
 
     assert mock_plan.call_args_list[0].kwargs == {
         "output_folder": "/user_provided_output_path/transfer",
