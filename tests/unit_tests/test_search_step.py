2cad23b5d2a6cdcced48624e7a1f197bf56c8cb9
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-01-20 23:36:58 +0100

    reactivate ruff also for tests

--- tests/unit_tests/test_search_step.py
+++ tests/unit_tests/test_search_step.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 from copy import deepcopy
+from unittest.mock import MagicMock, patch
 
 import pytest
 from alphabase.constants import _const
@@ -9,6 +10,7 @@ from alphabase.constants.modification import MOD_DF
 from alphadia import search_step
 from alphadia.search_step import SearchStep
 from alphadia.test_data_downloader import DataShareDownloader
+from alphadia.workflow.config import Config
 
 
 @pytest.mark.slow()
@@ -95,15 +97,9 @@ def test_custom_modifications():
     step = search_step.SearchStep(temp_directory, config=config)  # noqa F841
     assert "ThisModDoesNotExists@K" in MOD_DF["mod_name"].values
 
-import pytest
-from unittest.mock import MagicMock, patch
-from alphadia.workflow.config import Config
-
 
 @patch("alphadia.search_step.SearchStep._load_default_config")
-def test_initializes_with_default_config(
-    mock_load_default_config
-):
+def test_initializes_with_default_config(mock_load_default_config):
     """Test that the config is initialized with default values."""
     config = Config({"key1": "value1", "key2": "value2"}, "default")
     mock_load_default_config.return_value = deepcopy(config)
@@ -114,19 +110,22 @@ def test_initializes_with_default_config(
     mock_load_default_config.assert_called_once()
     assert result == config | {"output_directory": "/output"}
 
+
 @patch("alphadia.search_step.SearchStep._load_default_config")
-def test_updates_with_user_config_object(
-    mock_load_default_config
-):
+def test_updates_with_user_config_object(mock_load_default_config):
     """Test that the config is updated with user config object."""
     config = Config({"key1": "value1", "key2": "value2"})
     mock_load_default_config.return_value = deepcopy(config)
 
     user_config = Config({"key2": "value2b"})
-    #when
+    # when
     result = SearchStep._init_config(user_config, None, None, "/output")
 
-    assert result == {"key1": "value1", "key2": "value2b", "output_directory": "/output"}
+    assert result == {
+        "key1": "value1",
+        "key2": "value2b",
+        "output_directory": "/output",
+    }
 
 
 @patch("alphadia.search_step.SearchStep._load_default_config")
@@ -134,20 +133,32 @@ def test_updates_with_user_and_cli_and_extra_config_dicts(
     mock_load_default_config,
 ):
     """Test that the config is updated with user, cli and extra config dicts."""
-    config = Config({"key1": "value1", "key2": "value2", "key3": "value3", "key4": "value4", "output_directory": None})
+    config = Config(
+        {
+            "key1": "value1",
+            "key2": "value2",
+            "key3": "value3",
+            "key4": "value4",
+            "output_directory": None,
+        }
+    )
     mock_load_default_config.return_value = deepcopy(config)
 
     user_config = {"key2": "value2b"}
     cli_config = {"key3": "value3b"}
     extra_config = {"key4": "value4b"}
-    #when
+    # when
     result = SearchStep._init_config(user_config, cli_config, extra_config, "/output")
 
     mock_load_default_config.assert_called_once()
 
-    assert result == {"key1": "value1", "key2": "value2b", "key3": "value3b", "key4": "value4b", "output_directory": "/output"}
-
-
+    assert result == {
+        "key1": "value1",
+        "key2": "value2b",
+        "key3": "value3b",
+        "key4": "value4b",
+        "output_directory": "/output",
+    }
 
 
 @patch("alphadia.search_step.SearchStep._load_default_config")
@@ -159,12 +170,13 @@ def test_updates_with_cli_config_no_overwrite_output_path(
     mock_load_default_config.return_value = deepcopy(config)
 
     user_config = {"key1": "value1b", "output_directory": "/output"}
-    #when
+    # when
     result = SearchStep._init_config(user_config, None, None, "/another_output")
 
     mock_load_default_config.assert_called_once()
 
     assert result == {"key1": "value1b", "output_directory": "/output"}
+
 
 @patch("alphadia.search_step.SearchStep._load_default_config")
 def test_updates_with_extra_config_overwrite_output_path(
@@ -175,7 +187,7 @@ def test_updates_with_extra_config_overwrite_output_path(
     mock_load_default_config.return_value = deepcopy(config)
 
     extra_config = {"key1": "value1b"}
-    #when
+    # when
     result = SearchStep._init_config(None, None, extra_config, "/extra_output")
 
     mock_load_default_config.assert_called_once()
@@ -190,5 +202,5 @@ def test_raises_value_error_for_invalid_user_config(
     mock_load_default_config.return_value = MagicMock(spec=Config)
 
     with pytest.raises(TypeError, match="'str' object is not a mapping"):
-        #when
+        # when
         SearchStep._init_config("not_a_dict_nor_config_object", None, None, "/output")
