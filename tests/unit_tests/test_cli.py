166ff3b74572e2c1462b2e09314c4d9a5c8829b3
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-01-23 17:19:01 +0100

    format

--- tests/unit_tests/test_cli.py
+++ tests/unit_tests/test_cli.py
@@ -1,6 +1,7 @@
 """This module provides unit tests for alphadia.cli."""
 
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
+
 from alphadia.cli import run
 
 
@@ -15,10 +16,14 @@ def test_cli_minimal_args(mock_search_plan, mock_init_logging, mock_parse_known_
     # when
     run()
 
-    mock_search_plan.assert_called_once_with("/output", {}, {
-        'library_path': mock_args.library,
-                                                        'fasta_paths': mock_args.fasta,
-                                                        'quant_directory': mock_args.quant_dir}
-                                             )
+    mock_search_plan.assert_called_once_with(
+        "/output",
+        {},
+        {
+            "library_path": mock_args.library,
+            "fasta_paths": mock_args.fasta,
+            "quant_directory": mock_args.quant_dir,
+        },
+    )
 
     mock_init_logging.assert_called_once_with("/output")
