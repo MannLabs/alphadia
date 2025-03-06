671d752735c006429791705d03ff64ccbb6c5022
Georg Wallmann <6473148+GeorgWa@users.noreply.github.com>
2025-03-06 14:08:03 +0000

    Bump version: 1.9.3-dev7 → 1.10.0-dev0

--- gui/src/main/modules/profile.js
+++ gui/src/main/modules/profile.js
@@ -3,7 +3,7 @@ const path = require("path")
 const { app, shell, BrowserWindow} = require("electron")
 const { dialog } = require('electron')
 
-const VERSION = "1.9.3-dev7"
+const VERSION = "1.10.0-dev0"
 
 const Profile = class {
 
