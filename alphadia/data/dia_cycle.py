dbbcfb0111da1e2278e8957f8c72e6383638c80f
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-05-21 18:42:24 +0200

    feat: move DIA cycle determination to new dia_cycle module

---
+++ alphadia/data/dia_cycle.py
@@ -0,0 +1,173 @@
+"""Module to determine the DIA cycle from a spectrum dataframe."""
+import logging
+
+import numba as nb
+import numpy as np
+import pandas as pd
+from exceptions import NotValidDiaDataError
+
+
+logger = logging.getLogger()
+
+def normed_auto_correlation(x: np.ndarray):
+    """Calculate the normalized auto correlation of a 1D array.
+    Parameters
+    ----------
+
+    x : np.ndarray
+        The input array.
+
+    Returns
+    -------
+    np.ndarray
+        The normalized auto correlation of the input array.
+
+    """
+    x = x - x.mean()
+    result = np.correlate(x, x, mode="full")
+    result = result[len(result) // 2 :]
+    result /= result[0]
+    return result
+
+
+def get_cycle_length(cycle_signature: np.ndarray):
+    """Get the cycle length from the cycle signature.
+
+    Parameters
+    ----------
+
+    cycle_signature: np.ndarray
+        The signature of the DIA cycle. This will usually be the sum of the isolation windows.
+
+    Returns
+    -------
+
+    cycle_length: int
+        The length of the DIA cycle.
+    """
+
+    corr = normed_auto_correlation(cycle_signature)
+
+    is_peak = (corr[1:-1] > corr[:-2]) & (corr[1:-1] > corr[2:])
+
+    peak_index = is_peak.nonzero()[0] + 1
+    argmax = np.argmax(corr[peak_index])
+
+    return peak_index[argmax]
+
+
+@nb.njit
+def get_cycle_start(
+    cycle_signature: np.ndarray,
+    cycle_length: int,
+):
+    """Get the cycle start from the cycle signature.
+
+    Parameters
+    ----------
+
+    cycle_signature: np.ndarray
+        The signature of the DIA cycle. This will usually be the sum of the isolation windows.
+
+    cycle_length: int
+        The length of the DIA cycle.
+
+    Returns
+    -------
+
+    cycle_start: int
+        The index of the first cycle in the signature.
+    """
+    for i in range(len(cycle_signature) - (2 * cycle_length)):
+        if np.all(cycle_signature[i : i + cycle_length] == cycle_signature[i]):
+            continue
+
+        if np.all(
+            cycle_signature[i : i + cycle_length]
+            == cycle_signature[i + cycle_length : i + 2 * cycle_length]
+        ):
+            return i
+
+    return -1
+
+
+@nb.njit
+def is_valid_cycle(cycle_signature: np.ndarray, cycle_length: int, cycle_start: int):
+    """Return whether the found DIA cycle is valid.
+
+    Parameters
+    ----------
+
+    cycle_signature: np.ndarray
+        The signature of the DIA cycle. This will usually be the sum of the isolation windows.
+
+    cycle_length: int
+        The length of the DIA cycle.
+
+    cycle_start: int
+        The index of the first cycle in the signature.
+
+    Returns
+    -------
+
+    cycle_valid: bool
+        True if the cycle is valid, False otherwise.
+    """
+    for i in range(len(cycle_signature) - (2 * cycle_length) - cycle_start):
+        if not np.all(
+            cycle_signature[i + cycle_start : i + cycle_start + cycle_length]
+            == cycle_signature[
+                i + cycle_start + cycle_length : i + cycle_start + 2 * cycle_length
+            ]
+        ):
+            return False
+    return True
+
+
+def determine_dia_cycle(
+    spectrum_df: pd.DataFrame,
+    subset_for_cycle_detection: int = 10000,
+):
+    """Determine the DIA cycle and store it in self.cycle.
+
+    Parameters
+    ----------
+
+    spectrum_df : pandas.DataFrame
+        AlphaRaw compatible spectrum dataframe.
+
+    subset_for_cycle_detection : int, default = 10000
+        The number of spectra to use for cycle detection.
+
+    """
+    logger.info("Determining DIA cycle")
+
+    cycle_signature = (
+        spectrum_df.isolation_lower_mz.values[:subset_for_cycle_detection]
+        + spectrum_df.isolation_upper_mz.values[:subset_for_cycle_detection]
+    )
+    cycle_length = get_cycle_length(cycle_signature)
+
+    cycle_start = get_cycle_start(cycle_signature, cycle_length)
+
+    if cycle_start == -1:
+        raise NotValidDiaDataError("Failed to determine start of DIA cycle.")
+
+    if not is_valid_cycle(cycle_signature, cycle_length, cycle_start):
+        raise NotValidDiaDataError(
+            f"Cycle with start {spectrum_df.rt.values[cycle_start]:.2f} min and length {cycle_length} detected, but does not consistent."
+        )
+
+    logger.info(
+        f"Found cycle with start {spectrum_df.rt.values[cycle_start]:.2f} min and length {cycle_length}."
+    )
+
+    cycle = np.zeros((1, cycle_length, 1, 2), dtype=np.float64)
+    cycle[0, :, 0, 0] = spectrum_df.isolation_lower_mz.values[
+        cycle_start : cycle_start + cycle_length
+    ]
+    cycle[0, :, 0, 1] = spectrum_df.isolation_upper_mz.values[
+        cycle_start : cycle_start + cycle_length
+    ]
+
+    return cycle, cycle_start, cycle_length
