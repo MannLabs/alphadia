936858070102c8108a8a16dc029c963dba822cdb
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-05-21 14:55:26 +0200

    add tests

---
+++ tests/unit_tests/test_peptidecentric.py
@@ -0,0 +1,65 @@
+"""Unit test for the peptidecentric module."""
+
+from unittest.mock import MagicMock
+
+import pandas as pd
+import pytest
+from workflow.peptidecentric import PeptideCentricWorkflow
+
+
+@pytest.fixture
+def mock_config():
+    return {
+        "output_directory": "",
+        "calibration": {"min_correlation": 0.7, "max_fragments": 3},
+    }
+
+
+def test_filters_precursors_and_fragments_correctly(mock_config):
+    """Test that the filter_dfs method filters precursors and fragments correctly."""
+    precursor_df = pd.DataFrame(
+        {"qval": [0.005, 0.005, 0.011], "decoy": [0, 1, 0], "precursor_idx": [1, 2, 3]}
+    )
+    fragments_df = pd.DataFrame(
+        {
+            "precursor_idx": [1, 1, 1, 2, 1],
+            "mass_error": [1, 3, -2, 5, 200],
+            "correlation": [0.7, 0.5, 0.8, 0.6, 0.9],
+        }
+    )
+    instance = PeptideCentricWorkflow("test_instance", mock_config)
+    instance.reporter = MagicMock()
+
+    # when
+    filtered_precursors, filtered_fragments = instance.filter_dfs(
+        precursor_df, fragments_df
+    )
+
+    pd.testing.assert_frame_equal(
+        filtered_precursors,
+        pd.DataFrame(
+            {
+                "qval": [
+                    0.005,
+                ],
+                "decoy": [
+                    0,
+                ],
+                "precursor_idx": [
+                    1,
+                ],
+            }
+        ),
+    )
+
+    pd.testing.assert_frame_equal(
+        filtered_fragments.reset_index(drop=True),
+        pd.DataFrame(
+            {
+                "precursor_idx": [1, 1, 1],
+                "mass_error": [200, -2, 1],
+                "correlation": [0.9, 0.8, 0.7],
+            }
+        ),
+        check_like=True,
+    )
