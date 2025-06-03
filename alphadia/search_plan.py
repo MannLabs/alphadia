f6d084468ccd09ee45016896cf4f548701147e44
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-06-03 15:49:44 +0200

    move logging

--- alphadia/search_plan.py
+++ alphadia/search_plan.py
@@ -13,11 +13,11 @@ from alphadia.outputtransform.search_plan_output import (
     SearchPlanOutput,
 )
 from alphadia.reporting import reporting
+from alphadia.reporting.logging import print_environment, print_logo
 from alphadia.search_step import (
     SearchStep,
     logger,
 )
-from alphadia.utilities.logging import print_environment, print_logo
 
 # TODO the names of the steps need to be adjusted
 TRANSFER_STEP_NAME = "transfer"
