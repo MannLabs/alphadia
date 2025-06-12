d418e85f49d10109d35c4f3dee7586dd12170c42
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-06-12 14:38:28 +0200

    fix nb tests

--- tests/run_nb_tests.sh
+++ tests/run_nb_tests.sh
@@ -2,5 +2,5 @@
 
 ENV_NAME=${1:-alphadia}
 
-NBS=$(find ./nbs/tutorial_nbs -name "*.ipynb" | grep -v "finetuning.ipynb")  # exclude finetuning notebook for it takes too long
+NBS=$(find ../nbs/tutorial_nbs -name "*.ipynb" | grep -v "finetuning.ipynb")  # exclude finetuning notebook for it takes too long
 conda run -n $ENV_NAME --no-capture-output python -m pytest --nbmake $(echo $NBS)
