cea2978d3e7380e4677101c6653887111688fd7c
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-07-04 15:24:24 +0200

    simplify _norm_to_rt

--- alphadia/workflow/peptidecentric/library_init.py
+++ alphadia/workflow/peptidecentric/library_init.py
@@ -2,22 +2,23 @@ import numpy as np
 from alphabase.spectral_library.base import SpecLibBase
 
 from alphadia.reporting.reporting import Pipeline
-from alphadia.workflow.config import Config
 
 
 def init_spectral_library(
-    config: Config,
     dia_cycle: np.ndarray,
     dia_rt_values: np.ndarray,
     reporter: Pipeline,
     spectral_library: SpecLibBase,
+    channel_filter: str | None = None,
 ) -> None:
     """Initialize the spectral library.
 
+    Normalizes the normalized retention time values form the spectral library to the observed RT values.
+    Filters the spectral library based on the observed mz values.
+    Optionally filters the spectral library to only contain precursors from selected channels (if set in config).
+
     Parameters
     ----------
-    config : Config
-        Configuration object containing search and calibration parameters.
     dia_cycle : np.ndarray
         Array of DIA cycle values, used to determine the mz limits for filtering.
     dia_rt_values : np.ndarray
@@ -26,11 +27,9 @@ def init_spectral_library(
         Reporter object for logging messages.
     spectral_library : SpecLibBase
         Spectral library object containing precursor information, will be modified in place.
-
-
-    Normalizes the normalized retention time values form the spectral library to the observed RT values.
-    Filters the spectral library based on the observed mz values.
-    Optionally filters the spectral library to only contain precursors from selected channels (if set in config).
+    channel_filter : str, optional
+        Comma-separated string of channel numbers to filter the spectral library (column "channel") by.
+        If empty, no filtering is applied.
 
     Returns
     -------
@@ -41,7 +40,7 @@ def init_spectral_library(
     """
     # normalize RT
     spectral_library.precursor_df["rt_library"] = _norm_to_rt(
-        config, dia_rt_values, spectral_library.precursor_df["rt_library"].values
+        dia_rt_values, spectral_library.precursor_df["rt_library"].values
     )
 
     # filter based on precursor observability
@@ -62,10 +61,8 @@ def init_spectral_library(
     # filter spectral library to only contain precursors from allowed channels
     spectral_library.precursor_df_unfiltered = spectral_library.precursor_df.copy()
 
-    if config["search"]["channel_filter"] != "":
-        selected_channels = [
-            int(c) for c in config["search"]["channel_filter"].split(",")
-        ]
+    if channel_filter:
+        selected_channels = [int(c) for c in channel_filter.split(",")]
 
         spectral_library.precursor_df = spectral_library.precursor_df_unfiltered[
             spectral_library.precursor_df_unfiltered["channel"].isin(selected_channels)
@@ -77,12 +74,8 @@ def init_spectral_library(
 
 
 def _norm_to_rt(
-    config: Config,
     dia_rt_values: np.ndarray,
     norm_values: np.ndarray,
-    active_gradient_start: float | None = None,
-    active_gradient_stop: float | None = None,
-    mode: str | None = None,
 ) -> np.ndarray:
     """Convert normalized retention time values to absolute retention time values.
 
@@ -94,51 +87,16 @@ def _norm_to_rt(
     norm_values : np.ndarray
         Array of normalized retention time values from the spectral library.
 
-    active_gradient_start : float, optional
-        Start of the active gradient in seconds, by default None.
-        If None, it is set to the first retention time value plus half the configured `initial_rt_tolerance`.
-
-    active_gradient_stop : float, optional
-        End of the active gradient in seconds, by default None.
-        If None, it is set to the last retention time value minus half the configured `initial_rt_tolerance`.
-
-    mode : str, optional
-        Mode of the gradient, by default None.
-        If None, the value from the config is used which should be 'linear' by default
-
     Returns
     -------
     np.ndarray
         Array of absolute retention time values corresponding to the normalized values.
     """
-    # TODO: "initial retention time tolerance in seconds if > 1, or a proportion of the total gradient length if < 1" not reflected here!
-    # TODO: would expect the signs turned around?
-    lower_rt = (
-        (dia_rt_values[0] + config["search_initial"]["initial_rt_tolerance"] / 2)
-        if active_gradient_start is None
-        else active_gradient_start
-    )
-
-    upper_rt = (
-        dia_rt_values[-1] - (config["search_initial"]["initial_rt_tolerance"] / 2)
-        if active_gradient_stop is None
-        else active_gradient_stop
-    )
 
     # make sure values are really norm values
     norm_values = np.interp(norm_values, [norm_values.min(), norm_values.max()], [0, 1])
 
-    # determine the mode based on the config or the function parameter
-    mode = (
-        config["calibration"].get("norm_rt_mode", "linear")
-        if mode is None
-        else mode.lower()
-    )
+    lower_rt = dia_rt_values[0]
+    upper_rt = dia_rt_values[-1]
 
-    if mode == "linear":
-        return np.interp(norm_values, [0, 1], [lower_rt, upper_rt])
-
-    if mode == "tic":
-        raise NotImplementedError("tic mode is not implemented yet")
-
-    raise ValueError(f"Unknown norm_rt_mode {mode}")
+    return np.interp(norm_values, [0, 1], [lower_rt, upper_rt])
