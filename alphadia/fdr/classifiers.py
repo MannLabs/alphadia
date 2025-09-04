1b144f707a0740e19840258d6b83ac8b18c59b5c
GeorgWa <wallmann@biochem.mpg.de>
2025-09-04 23:47:13 +0200

    disable norm and use batch norm

--- alphadia/fdr/classifiers.py
+++ alphadia/fdr/classifiers.py
@@ -355,9 +355,6 @@ class BinaryClassifierLegacyNewBatching(Classifier):
                 dropout=self.dropout,
             )
 
-        # normalize input
-        x = (x - x.mean(axis=0)) / (x.std(axis=0) + 1e-6)
-
         if y.ndim == 1:
             y = np.stack([1 - y, y], axis=1)
 
@@ -379,6 +376,9 @@ class BinaryClassifierLegacyNewBatching(Classifier):
         )
 
         loss = nn.BCELoss()
+
+        # Set model to training mode for BatchNorm
+        self.network.train()
 
         x_train = torch.Tensor(x_train)
         y_train = torch.Tensor(y_train)
@@ -477,7 +477,6 @@ class BinaryClassifierLegacyNewBatching(Classifier):
             x.shape[1] == self.input_dim
         ), "Input data must have the same number of features as the fitted classifier."
 
-        x = (x - x.mean(axis=0)) / (x.std(axis=0) + 1e-6)
         self.network.eval()
         return np.argmax(self.network(torch.Tensor(x)).detach().numpy(), axis=1)
 
@@ -506,7 +505,6 @@ class BinaryClassifierLegacyNewBatching(Classifier):
             x.shape[1] == self.input_dim
         ), "Input data must have the same number of features as the fitted classifier."
 
-        x = (x - x.mean(axis=0)) / (x.std(axis=0) + 1e-6)
         self.network.eval()
         return self.network(torch.Tensor(x)).detach().numpy()
 
