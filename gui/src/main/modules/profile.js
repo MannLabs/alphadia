08e076da30651c66a29d247b19cd9731e7a8a7e7
null <82171591+mschwoer@users.noreply.github.com>
2025-10-02 11:54:01 +0000

    Bump version: 1.11.1-dev0 → 1.12.0-dev0

--- gui/src/main/modules/profile.js
+++ gui/src/main/modules/profile.js
@@ -3,7 +3,7 @@ const path = require("path")
 const { app, shell, BrowserWindow} = require("electron")
 const { dialog } = require('electron')
 
-const VERSION = "1.11.1-dev0"
+const VERSION = "1.12.0-dev0"
 
 const Profile = class {
 
