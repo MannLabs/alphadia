fc867631abc8068066113840a2ac8d8bc9c65a26
null <82171591+mschwoer@users.noreply.github.com>
2025-10-02 11:54:01 +0000

    Bump version: 1.12.0-dev0 → 1.12.0

--- alphadia/__init__.py
+++ alphadia/__init__.py
@@ -2,7 +2,7 @@
 
 import warnings
 
-__version__ = "1.12.0-dev0"
+__version__ = "1.12.0"
 
 warnings.filterwarnings(
     "ignore", message="Dependency 'dask' not installed.", module="directlfq"
--- gui/src/main/modules/profile.js
+++ gui/src/main/modules/profile.js
@@ -3,7 +3,7 @@ const path = require("path")
 const { app, shell, BrowserWindow} = require("electron")
 const { dialog } = require('electron')
 
-const VERSION = "1.12.0-dev0"
+const VERSION = "1.12.0"
 
 const Profile = class {
 
