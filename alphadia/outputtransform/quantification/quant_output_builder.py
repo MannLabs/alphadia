b298427e95fbbcc6d5adbcec559ed09d39170a3f
Tung <tungvuduc@outlook.de>
2025-11-05 16:08:25 +0100

    feat: integrate normalization method into LFQOutputConfig for all quantification levels

--- alphadia/outputtransform/quantification/quant_output_builder.py
+++ alphadia/outputtransform/quantification/quant_output_builder.py
@@ -165,6 +165,9 @@ class QuantOutputBuilder:
                 save_fragments=self.config["search_output"][
                     "save_fragment_quant_matrix"
                 ],
+                normalization_method=self.config["search_output"][
+                    "normalization_method"
+                ],
             ),
             LFQOutputConfig(
                 quant_level=QuantificationLevelKey.PEPTIDE,
@@ -180,6 +183,9 @@ class QuantOutputBuilder:
                 save_fragments=self.config["search_output"][
                     "save_fragment_quant_matrix"
                 ],
+                normalization_method=self.config["search_output"][
+                    "normalization_method"
+                ],
             ),
             LFQOutputConfig(
                 quant_level=QuantificationLevelKey.PROTEIN,
@@ -189,6 +195,9 @@ class QuantOutputBuilder:
                     QuantificationLevelName.PROTEIN,
                 ],
                 should_process=True,
+                normalization_method=self.config["search_output"][
+                    "normalization_method"
+                ],
             ),
         ]
 
