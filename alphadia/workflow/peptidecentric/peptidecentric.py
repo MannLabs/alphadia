ee111b07486a2e20001c0a492929a72ffce8baa4
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-03-30 11:02:12 +0200

    remove obsolete addition

--- alphadia/workflow/peptidecentric/peptidecentric.py
+++ alphadia/workflow/peptidecentric/peptidecentric.py
@@ -253,11 +253,6 @@ class PeptideCentricWorkflow(base.WorkflowBase):
                 )
             )
 
-            # TODO: check: is this required?
-            if candidates_fdr_df.empty:
-                log_precursor_df(self.reporter, precursor_fdr_df)
-                return pd.DataFrame(), pd.DataFrame()
-
             precursor_df, fragments_df = extraction_handler.quantify_candidates(
                 candidates_fdr_df,
                 precursor_fdr_df,
